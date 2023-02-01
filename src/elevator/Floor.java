be6dce983ab2b2ded2df49bb7725a02da1e01476
Filip Lukic <filiplukic000@gmail.com>
2023-02-01 14:59:11 -0500

    Implment sendMessage

--- src/elevator/Floor.java
+++ src/elevator/Floor.java
@@ -1,16 +1,21 @@
 package elevator;
 
+import java.util.ArrayList;
+import java.time.LocalTime;
+
 public class Floor implements Runnable{
 	private Scheduler server;
-	private int floorNumber;
+	private final int floorNumber;
+	private final ArrayList<Integer> elevatorNumbers;
 	
-	public Floor(Scheduler server, int floorNumber) {
+	public Floor(Scheduler server, int floorNumber, ArrayList<Integer> elevatorNumbers) {
 		this.server = server;
 		this.floorNumber = floorNumber;
+		this.elevatorNumbers = elevatorNumbers;
 	}
 	
-	private void sendMessage() {
-		
+	private void sendMessage(boolean goingUp) {
+		server.handleMessage(new Message(LocalTime.now(), this.floorNumber, goingUp));
 	}
 	
 	public void run() {
