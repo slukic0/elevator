13e8d31fbee3227f8fd40961e7089e5e16cda553
Filip Lukic <filiplukic000@gmail.com>
2023-02-01 15:44:25 -0500

    Add message queue with get and add methods - Change handleMessage signature

--- src/elevator/Scheduler.java
+++ src/elevator/Scheduler.java
@@ -1,15 +1,27 @@
 package elevator;
 
+import java.util.ArrayList;
+import java.util.Queue;
+
 public class Scheduler implements Runnable{
 	private Floor[] floors;
 	private Elevator[] elevators;
+	private static Queue<Message> messageQueue;
 	
 	public Scheduler(Floor[] floors, Elevator[] elevators) {
 		this.floors = floors;
 		this.elevators = elevators;
 	}
 	
-	public void handleMessage(Message message) {
+	public static Queue<Message> getMessages() {
+		return messageQueue;
+	}
+	
+	public static void enqueueMessage(Message mesage) {
+		messageQueue.add(mesage);
+	}
+	
+	public void handleFloorMessage(Message message) {
 		// TODO: implement handleMessage
 	}
 	
