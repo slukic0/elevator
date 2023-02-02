98c76efcf0a47a56aa36f83b4b8e635d09694761
Pranav Hari <pranavbts2001@gmail.com>
2023-02-01 19:09:09 -0500

    Add sendMessage function

--- src/elevator/Elevator.java
+++ src/elevator/Elevator.java
@@ -1,14 +1,33 @@
 package elevator;
 
+import elevator.*;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
 public class Elevator implements Runnable {
 	private Scheduler server;
+	private ArrayList<Integer> floorNums;
+	private int currentFloor;
 	
-	public Elevator(Scheduler server) {
+	public Elevator(Scheduler server, ArrayList<Integer> floorNums, int currentFloor) {
 		this.server = server;
+		this.floorNums = floorNums;
+		this.currentFloor = currentFloor;
 	}
 	
 	public void run() {
 		return;
 	}
-
+	
+	public void sendMessage(boolean goingUp) {
+		
+		synchronized (server) {
+			Scheduler.enqueueMessage(new Message(LocalTime.now(), this.currentFloor, goingUp));
+		}
+		
+	}
+	
+	public void setCurrentFloor(int floor) {
+		this.currentFloor = floor;
+	}
 }
